ea2d4d44a93ee5ad8832de51d1745477ef9decf1
Titus Tienaah <dxtitus@gmail.com>
2016-09-11 03:47:58 -0300

    added srs test

--- prj_test.go
+++ prj_test.go
@@ -1,26 +1,38 @@
 package prj
 
 import (
-	. "github.com/franela/goblin"
-	"testing"
-	"math"
+    . "github.com/franela/goblin"
+    "testing"
+    "math"
 )
 
 func TestCart(t *testing.T) {
-	g := Goblin(t)
+    g := Goblin(t)
 
-	g.Describe("geom.point", func() {
-		g.It("x, y access & null", func() {
-			lng, lat := 28.00372, 40.81747
+    g.Describe("project point", func() {
+        g.It("srs", func() {
+            var srs = NewSRS(4326).AsGeographic()
+            var srs_prjs = NewSRS(3857).AsProjected()
 
-			x, y, err := NewSRS(4326).AsGeographic().To(
-				NewSRS(3857).AsProjected(),
-			).Trans(lng, lat)
+            g.Assert(srs.isGeog()).IsTrue()
+            g.Assert(srs.isProj()).IsFalse()
 
-			g.Assert(err == nil).IsTrue()
-			g.Assert(math.Abs(x - 3117359.85071741) < 1.0e-8).IsTrue()
-			g.Assert(math.Abs(y - 4985455.69596696) < 1.0e-8).IsTrue()
-		})
+            g.Assert(srs_prjs.isProj()).IsTrue()
+            g.Assert(srs_prjs.isGeog()).IsFalse()
 
-	})
+        })
+
+        g.It("transformation x,y", func() {
+            lng, lat := 28.00372, 40.81747
+
+            x, y, err := NewSRS(4326).AsGeographic().To(
+                NewSRS(3857).AsProjected(),
+            ).Trans(lng, lat)
+
+            g.Assert(err == nil).IsTrue()
+            g.Assert(math.Abs(x - 3117359.85071741) < 1.0e-8).IsTrue()
+            g.Assert(math.Abs(y - 4985455.69596696) < 1.0e-8).IsTrue()
+        })
+
+    })
 }
